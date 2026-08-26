5d455e49f1db5ce2614b3a6b9da06b5fbb82f85f
Dasun Pubudumal <pubudumald@gmail.com>
2026-08-26 09:01:02 +0100

    update oil conifg

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -10,7 +10,53 @@ return {
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
     config = function()
-      require("oil").setup({})
+      require("oil").setup({
+        view_options = {
+          -- Show files and directories that start with "."
+          show_hidden = true,
+          -- This function defines what is considered a "hidden" file
+          is_hidden_file = function(name, bufnr)
+            local m = name:match("^%.")
+            return m ~= nil
+          end,
+          -- This function defines what will never be shown, even when `show_hidden` is set
+          is_always_hidden = function(name, bufnr)
+            return name == ".git" or name == ".."
+          end,
+          -- Sort file names with numbers in a more intuitive order for humans.
+          -- Can be "fast", true, or false. "fast" will turn it off for large directories.
+          natural_order = "fast",
+          -- Sort file and directory names case insensitive
+          case_insensitive = false,
+          sort = {
+            -- sort order can be "asc" or "desc"
+            -- see :help oil-columns to see which columns are sortable
+            { "type", "asc" },
+            { "name", "asc" },
+          },
+          -- Customize the highlight group for the file name
+          highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+            return nil
+          end,
+        },
+        -- Extra arguments to pass to SCP when moving/copying files over SSH
+        extra_scp_args = {},
+        -- Extra arguments to pass to aws s3 when creating/deleting/moving/copying files using aws s3
+        extra_s3_args = {},
+        -- EXPERIMENTAL support for performing file operations with git
+        git = {
+          -- Return true to automatically git add/mv/rm files
+          add = function(path)
+            return false
+          end,
+          mv = function(src_path, dest_path)
+            return false
+          end,
+          rm = function(path)
+            return false
+          end,
+        },
+      })
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
   },
